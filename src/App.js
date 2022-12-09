b0711b1181ffe9e398a55ee8ecce44d6c365fae1
park <white1614@naver.com>
2022-12-09 17:52:20 +0900

    todolist 완성

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
     }
   ]);
 
-  const nextId = useRef(4)
+  const nextId = useRef(4);
 
   const onInsert = useCallback(
     text => {
@@ -37,10 +37,26 @@ const App = () => {
     [todos],
   )
 
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id!== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        todos.map(todo => todo.id === id ? {...todo, checked: !todo.checked} : todo,),
+      );
+    },
+    [todos],
+  )
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList todos = {onInsert}/>
+      <TodoInsert onInsert = {onInsert}/>
+      <TodoList todos = {todos} onRemove = {onRemove} onToggle = {onToggle}/>
     </TodoTemplate>
   );
 };
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -2,14 +2,27 @@ import { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss'
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
     const [value, setValue] = useState('');
 
-    const onChange = useCallback(x => {
-        setValue(x.target.value);
-    },[])
+    const onChange = useCallback(
+        x=> {
+            setValue(x.target.value);
+        },
+        []
+    );
+
+    const onSubmit = useCallback(
+        x => {
+        onInsert(value);
+        setValue('');
+        x.preventDefault();
+        }
+        ,[onInsert,value]
+    );
+
     return (
-        <form className='TodoInsert'>
+        <form className='TodoInsert' onSubmit={onSubmit}>
             <input placeholder='할 일을 입력하세요.'
             value={value}
             onChange={onChange}
